35522bf9f5977fec2bf38a58b5f2c89ea1699475
juliocpmelo <julio.melo@imd.ufrn.br>
2021-06-30 12:16:42 -0300

    Removendo build, adicionando correções e comentários

--- inverter/src/function.cpp
+++ inverter/src/function.cpp
@@ -1,11 +1,15 @@
-#include "function.h"
+//#include "function.h"
 
 #include <iterator>
 using std::iter_swap;
+#include <array>
 
 /*! 
  * Reverse de order of elements inside the array.
  * @param arr Reference to the array with the values.
+ */
+ /*
+ erro de compilação, não vou considerar
  */
 template <size_t SIZE>
 void reverse( std::array< std::string, SIZE > & arr )
@@ -16,6 +20,7 @@ void reverse( std::array< std::string, SIZE > & arr )
 
     for(itr; itr != arr.end() - 1; itr++)
     {
-      iter_swap(itr, (arr.end() - itr - 1));
+      iter_swap(itr, (arr.end() - itr - 1)); //não era exatametne assim que vc usaria, o iter_swap troca dois iteradores, vc está trocando um iterador (itr) com um número (arr.end() - itr - 1)
+
     }
 }
--- minmax/src/function.cpp
+++ minmax/src/function.cpp
@@ -10,6 +10,11 @@
  * @return A pair of indexes to the first smallest and last largest values.
  */
 
+/*
+50%
+o primeiro caso de teste falha pq vc deveria retornar {-1,-1}
+os dois últimos casos de teste falham pq vc acaba retornando a posição errada do elemento, por causa da ordenação
+*/
 std::pair<int,int> min_max( int V[], std::size_t n )
 {
     // TODO: Adicione aqui sua solução.
@@ -25,7 +30,7 @@ std::pair<int,int> min_max( int V[], std::size_t n )
       vAux[i] = V[i];
     }
 
-    for(int i = 0; i < n - 1; i++)
+    for(int i = 0; i < n - 1; i++) //não precisava ordenar o vetor pra achar o maior valor
     {
       for(int j = i + 1; j < n; j++)
       {
